1f914fb2e30da741bac5009052025328d8e7d288
Michael Kerr <bulluffalo@gmail.com>
2022-10-07 12:55:38 -0700

    remove mobile_other_url

--- index.js
+++ index.js
@@ -18,13 +18,6 @@ window.onload = (event) => {
             redirectUrl = params.ios_url
         }
     }
-    if ("mobile_other_url" in params) {
-        console.log("mobile_other_url Found")
-        if (/webOS|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-            console.log("Detected Mobile Other: ", navigator.userAgent)
-            redirectUrl = params.mobile_other_url
-        }
-    }
     if ("mobile_url" in params) {
         console.log("mobile_url Found")
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
